bb61fa13e7a66171008efb0ba1425b4e2e8b96d2
Harshit Singhal <singhalharshit2910@gmail.com>
2025-06-22 22:59:35 +0530

    basic problems

---
+++ Practice_Questions_For_basics.py
@@ -0,0 +1,73 @@
+"""
+# 1. Create a list of 10 integers. Print the first and last elements
+
+
+l=[1,2,3,4,5,6,7,8,9,0]
+
+print(l[0],l[-1])
+
+"""
+
+
+"""
+# 2. Add 100 to the end of the list using append().
+
+l=[1,2,3,4]
+l.append(100)
+print(l)
+
+"""
+
+
+"""
+# 3. Insert 55 at position 5.
+
+l=[10,11,12,13,14,15,16,17,18,19,20]
+l.insert(5,55)
+print(l)
+
+"""
+
+"""
+# 4. Delete the 3rd item in the list using del.
+
+l=[10,11,12,13,14,15,16,17,18,19,20]
+del l[3]
+print(l)
+
+"""
+
+
+"""
+# 5. Remove the number 55 from the list using remove().
+
+l = [10,11,12,13,14,55,15,16,17,18,19,20]
+l.remove(55)
+print(l)
+
+"""
+
+
+"""
+# 6. Pop the last item and print it.
+
+l = [10,11,12,13,14,55,15,16,17,18,19,20]
+print(l.pop())
+
+"""
+
+
+"""
+# 7. Reverse the list using slicing.
+
+l = [10,11,12,13,14,55,15,16,17,18,19,20]
+l1=l[::-1]
+print(l1)
+
+"""
+
+# 8. Slice the list to get the middle 4 elements.
+l = [11,12,13,14,15,16,17,18,19,20]
+
+for i in range(len(l)//2):
+    l[]
