2383c76f57ea4b0c92d1c4234b621d2fd31a2b8c
Harshit Singhal <singhalharshit2910@gmail.com>
2025-06-22 18:53:04 +0530

    frequency_map

---
+++ frequency_map.py
@@ -0,0 +1,17 @@
+"""
+    To store a frequency into dictionary
+"""
+
+# Method 1 - Iterating over an empty dict if a key is already in dict increase the count else add the key 
+
+
+sample_list = [4, 2, 7, 4, 9, 2, 7, 4, 1, 3, 9, 2, 6, 7, 3]
+frequency_map ={}
+count = 0
+for i in range(0,len(sample_list)):
+    if sample_list[i] in frequency_map:
+        frequency_map[sample_list[i]]+=1
+    else:
+        frequency_map[sample_list[i]]=1
+
+print(frequency_map)
