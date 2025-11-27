1671312dbf66f0e0789c76d22b7b6c10153edfe3
Harshit Singhal <singhalharshit2910@gmail.com>
2025-11-28 01:24:27 +0530

    More in Counting of digits

--- counting_of_digits.py
+++ counting_of_digits.py
@@ -5,3 +5,16 @@ while num>0:
     num = num//10
     count+=1
 print(count)
+
+
+
+from math import *
+
+n1=123456
+
+def count_of_digits(n):
+    num =n
+    return(int(log10(num)+1))
+
+
+print(count_of_digits(n1))
